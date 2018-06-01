711e611064e91ac26227d737daa7c3338cfa6eb2
MBeliou <maxime.bele@gmail.com>
2018-06-01 17:19:20 +0200

    First Commit

---
+++ coinwarz.ts
@@ -0,0 +1,46 @@
+import fetch from "node-fetch";
+
+class Coinwarz {
+    apiKey: string;
+    urlBase: string = "http://www.coinwarz.com/v1/api/";
+
+    constructor(apiKey: string){
+        this.apiKey = apiKey;
+    }
+
+    async infoKey(){
+        const url = `${this.urlBase}apikeyinfo/?apikey=${this.apiKey}`;
+        try {
+            let ret = await this.queryURL(url);
+            return ret;
+        } catch (e) {
+            console.log('Error', e);
+        }
+    }
+
+    async miningProfitability(){
+        const url = `${this.urlBase}profitability/?apikey=${this.apiKey}&algo=all`;
+        try {
+            let ret = await this.queryURL(url);
+            return ret;
+        } catch (e) {
+            console.log('Error', e)
+        }
+    }
+
+    async coinInformation(coinTag: string = "BTC"){
+        const url = `${this.urlBase}coininformation/?apikey=${this.apiKey}&coinTag=${coinTag}`
+        try {
+            let ret = await this.queryURL(url);
+            return ret;
+        } catch(e) {
+            console.log("Error", e);
+        }
+    }
+
+    async queryURL(url: string){
+        let resp = await fetch(url);
+        let data = await resp.json();
+        return data
+    }
+}
